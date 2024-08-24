1f3b1ed92060afba9560985113861d43e64baa05
agustin9700 <ryuk10xd@gmail.com>
2024-08-24 20:33:46 -0300

    up

---
+++ controllers/apiClanWar2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+module.exports = function(req, res) {
+  fs.readFile('resultado_resta2.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error al leer el archivo JSON');
+      return;
+    }
+    const jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
+};
